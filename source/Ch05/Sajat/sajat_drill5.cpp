7a006cfaf71e22d61f155d4a40451ceb05a8fdbd
Barna <barnus987@gmail.com>
2020-10-07 13:31:23 +0200

    Kesz

---
+++ source/Ch05/Sajat/sajat_drill5.cpp
@@ -0,0 +1,36 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	//cout << " Success!\n ";
+	//cout << " Success!\n";
+	//cout << " Success " << "!\n ";
+	//cout << "success" << '\n';
+	//int res = 7; vector<int> v(10); v[5] = res; cout << " Success!\n " ;
+	//vector<int> v(10); v[5] = 7; if (v[5]==7) cout << " Success!\n " ;
+	//bool cond=true; if (cond) cout << " Success!\n " ; else cout << " Fail!\n " ;
+	//bool c = true; if (c) cout << " Success!\n " ; else cout << " Fail!\n " ;
+	//string s = " ape " ; bool c =true; if (c) cout << " Success!\n " ;
+	//string s = " fool " ; if (s== " fool " ) cout << " Success!\n " ;
+	//string s = " fool " ; if (s== " fool " ) cout << " Success!\n " ;
+	//string s = " ape " ; if (s!= " fool " ) cout << " Success!\n " ;
+	//vector<char> v(5); for (int i=0; i<v.size(); ++i) ; cout << " Success!\n " ;
+	//vector<char> v(5); for (int i=0; i<=v.size(); ++i) ; cout << " Success!\n " ;
+	//string s = " Success!\n " ; for (int i=0; i<9; ++i) cout << s[i];
+	//if (true) cout << " Success!\n " ; else cout << " Fail!\n " ;
+	//int x = 2000; int c = x; if (c==2000) cout << " Success!\n " ;
+	//string s = " Success!\n " ; for (int i=0; i<9; ++i) cout << s[i];
+	//vector<int> v(5); for (int i=0; i<=v.size(); ++i) ; cout << " Success!\n " ;
+	//int i=0; int j = 9; while (i<10) ++i; if (j<i) cout << " Success!\n " ;
+	//double x = 4; double d = 17/(x-2); if (d==2*x+0.5) cout << " Success!\n " ; 
+	//string s = " Success!\n " ; for (int i=0; i<=10; ++i) cout << s[i];
+	//int i=0; int j=9; while (i<10) ++i; if (j<i) cout << " Success!\n " ;
+	//double x = 4; double d = 17/(x-2); if(d==(2*x)+0.5)cout << " Success!\n " ;
+	//cout << " Success!\n " ;
+
+
+
+
+
+	return 0;
+}

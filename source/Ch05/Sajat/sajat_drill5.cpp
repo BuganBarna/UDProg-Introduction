845c15ec2213228c7a66af01d8e305b60bea518c
Barna <barnus987@gmail.com>
2020-10-09 21:19:27 +0200

    Upload_2.0

--- source/Ch05/Sajat/sajat_drill5.cpp
+++ source/Ch05/Sajat/sajat_drill5.cpp
@@ -1,7 +1,7 @@
 #include "std_lib_facilities.h"
 
 int main()
-{
+try{
 	//cout << " Success!\n ";
 	//cout << " Success!\n";
 	//cout << " Success " << "!\n ";
@@ -29,8 +29,15 @@ int main()
 	//cout << " Success!\n " ;
 
 
-
-
-
 	return 0;
+}
+catch (exception& e) {
+	cerr << "error: " << e.what() << '\n';
+	keep_window_open();
+	return 1;
+}
+catch (...) {
+	cerr << "Oops: unknown exception!\n";
+	keep_window_open();
+	return 2;
 }

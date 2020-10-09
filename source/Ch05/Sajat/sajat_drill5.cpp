5376b48eeb8d439b2bd282da9cad51b3c1405de0
Barna <barnus987@gmail.com>
2020-10-09 21:02:05 +0200

    Upload

--- source/Ch05/Sajat/sajat_drill5.cpp
+++ source/Ch05/Sajat/sajat_drill5.cpp
@@ -10,9 +10,9 @@ int main()
 	//vector<int> v(10); v[5] = 7; if (v[5]==7) cout << " Success!\n " ;
 	//bool cond=true; if (cond) cout << " Success!\n " ; else cout << " Fail!\n " ;
 	//bool c = true; if (c) cout << " Success!\n " ; else cout << " Fail!\n " ;
-	//string s = " ape " ; bool c =true; if (c) cout << " Success!\n " ;
-	//string s = " fool " ; if (s== " fool " ) cout << " Success!\n " ;
-	//string s = " fool " ; if (s== " fool " ) cout << " Success!\n " ;
+	//string s = " ape " ; bool c ="fool"<s; if (!c) cout << " Success!\n " ;
+	//string s = " ape " ; if (s!= " fool " ) cout << " Success!\n " ;
+	//string s = " ape " ; if (s!= " fool " ) cout << " Success!\n " ;
 	//string s = " ape " ; if (s!= " fool " ) cout << " Success!\n " ;
 	//vector<char> v(5); for (int i=0; i<v.size(); ++i) ; cout << " Success!\n " ;
 	//vector<char> v(5); for (int i=0; i<=v.size(); ++i) ; cout << " Success!\n " ;
@@ -25,7 +25,7 @@ int main()
 	//double x = 4; double d = 17/(x-2); if (d==2*x+0.5) cout << " Success!\n " ; 
 	//string s = " Success!\n " ; for (int i=0; i<=10; ++i) cout << s[i];
 	//int i=0; int j=9; while (i<10) ++i; if (j<i) cout << " Success!\n " ;
-	//double x = 4; double d = 17/(x-2); if(d==(2*x)+0.5)cout << " Success!\n " ;
+	//double x = 4; double d = 5.0/(x-2); if(d==x/2.0+0.5)cout << " Success!\n " ;
 	//cout << " Success!\n " ;
 
 
